0ca701668ff6533db6d6ca2018eeb32fec05a0f6
francids <francisco.mc.ids@gmail.com>
2024-08-14 23:31:46 -0400

    feat: Add endpoint for root path This commit adds a new endpoint for the root path ("/") in the main.go file. The endpoint returns a JSON response with a greeting and description for the store-api.

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"greeting":    "Welcome to store-api",
+			"description": "This is a simple store-api built with Go and Gin",
+		})
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run()
 }
